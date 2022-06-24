1c366a2c36f7ed65c408d38f0f0dffc1c5d083c6
Suz Oliver <suz.lynn.oliver@gmail.com>
2022-06-24 23:00:13 +0000

    Added a custom dropdown.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DropdownComponent } from './component/dropdown/dropdown.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DropdownComponent
   ],
   imports: [
     BrowserModule,
